580dddc9d9b195e4e6ad953464b0a762a66154e0
Lucas Araujo <kirilucas@gmail.com>
2021-09-17 16:36:39 -0300

    Changed layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react'
+import { Grid, TextField, Button } from '@mui/material';
+import React, { useState } from 'react'
 import './App.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+function App() {
+  const [formData, setFormData] = useState({})
+
+  const onChange = ({ target: { name, value } }) => {
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
   }
 
-  render() {
-    return (
-      <div></div>
-    )
-  }
+  console.log(formData)
+
+  return (
+    <div class="App">
+      <div class="container">
+        <Grid container spacing={1}>
+          <Grid item sm={8}>
+            <TextField name="username" label="Username" variant="standard" onChange={onChange} />
+          </Grid>
+          <Grid item sm={8}>
+            <TextField name="password" label="Password" type="password" variant="standard" onChange={onChange} />
+          </Grid>
+          <Grid item sm={8}>
+            <TextField name="email" label="E-mail" variant="standard" />
+          </Grid>
+          <Grid item sm={4}>
+          </Grid>
+          <Grid item sm={4}>
+            <Button variant="contained" fullWidth >Submit</Button>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
+
+export default App;
