9b5b77bb5541cd53eaad788e68c1a219681c1681
Lucas Araujo <kirilucas@gmail.com>
2021-09-21 15:48:04 -0300

    Added main icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Grid, TextField, Button } from '@mui/material';
 import React, { useState } from 'react'
+import leftEye from './left-eye.svg';
+import rightEye from './right-eye.svg';
 import './App.css';
 
 function App() {
@@ -12,29 +14,42 @@ function App() {
     }))
   }
 
-  console.log(formData)
 
   return (
-    <div class="App">
-      <div class="container">
-        <Grid container spacing={1}>
-          <Grid item sm={8}>
-            <TextField name="username" label="Username" variant="standard" onChange={onChange} />
-          </Grid>
-          <Grid item sm={8}>
-            <TextField name="password" label="Password" type="password" variant="standard" onChange={onChange} />
-          </Grid>
-          <Grid item sm={8}>
-            <TextField name="email" label="E-mail" variant="standard" />
-          </Grid>
-          <Grid item sm={4}>
-          </Grid>
-          <Grid item sm={4}>
-            <Button variant="contained" fullWidth >Submit</Button>
-          </Grid>
+
+    <div>
+      <Grid container spacing={1} class="potato">
+        <Grid item sm={4}>
+          <img class="eye left-eye" src={leftEye} />
         </Grid>
+        <Grid item sm={4}>
+          <img class="eye right-eye" src={rightEye} />
+        </Grid>
+      </Grid>
+
+        {
+        //* Sign up form 
+        }
+
+        
+        {/* <Grid container spacing={1} class="potato">
+          <Grid item sm={12}>
+            <TextField name="username" label="Username" variant="standard" onChange={onChange} fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField name="password" label="Password" type="password" variant="standard" onChange={onChange} fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField name="email" label="E-mail" variant="standard" onChange={onChange} fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+          </Grid>
+          <Grid item sm={12} sx={{ 'padding-top': '3vh' }} >
+            <Button variant="" fullWidth
+              disabled={!formData.username || !formData.password || !formData.email}>Sign In</Button>
+          </Grid>
+        </Grid> */}
       </div>
-    </div>
   );
 }
 
