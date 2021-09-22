9f08bb468a8ce68286ba82d7639c25a32544b5dc
Lucas Araujo <kirilucas@gmail.com>
2021-09-21 21:03:53 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Grid, TextField, Button } from '@mui/material';
 import React, { useState } from 'react'
 import leftEye from './left-eye.svg';
 import rightEye from './right-eye.svg';
-import { SignUpForm } from './signUp';
+import SignUpForm from './signUp.js';
 
 import './App.css';
 
 function App() {
   const [formData, setFormData] = useState({})
+  const [renderLoginModal, setRenderLoginModal] = useState(false)
 
   const onChange = ({ target: { name, value } }) => {
     setFormData(prev => ({
@@ -16,15 +17,15 @@ function App() {
     }))
   }
 
-  const teste = () => {
-    console.log('aaaaaaa')
+  const onClick = () => {
+    setRenderLoginModal(true)
   }
 
   return (
-
     <div>
+      <SignUpForm renderLoginModal={onClick}/>
       <Grid container spacing={1}>
-        <Grid item sm={6} onClick={teste}>
+        <Grid item sm={6} onClick={onClick}>
           <img class="eye left-eye" src={leftEye}/>
           {/* <span>teste</span> */}
         </Grid>
--- src/signUp.js
+++ src/signUp.js
@@ -1,33 +1,67 @@
 import { Grid, TextField, Button } from '@mui/material';
 import React, { useState } from 'react'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import './App.css';
 
-function SignUpForm() {
+function SignUpForm(props) {
   const [formData, setFormData] = useState({})
+  const [open, setOpen] = React.useState(props.renderLoginModal);
 
-  const onChange = ({ target: { name, value } }) => {
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
     setFormData(prev => ({
       ...prev,
       [name]: value
     }))
   }
 
-  const teste = () => {
-    console.log('aaaaaaa')
-  }
-
   return (
-
-    <div>        
-        <Grid container spacing={1} class="potato">
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Create new account</DialogTitle>
+        <DialogContent>
+          {/* <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText> */}
           <Grid item sm={12}>
-            <TextField name="username" label="Username" variant="standard" onChange={onChange} fullWidth />
+            <TextField name="username" label="Username" variant="standard" onChange={handleChange} fullWidth />
           </Grid>
           <Grid item sm={12}>
-            <TextField name="password" label="Password" type="password" variant="standard" onChange={onChange} fullWidth />
+            <TextField name="password" label="Password" type="password" variant="standard" onChange={handleChange} fullWidth />
           </Grid>
           <Grid item sm={12}>
-            <TextField name="email" label="E-mail" variant="standard" onChange={onChange} fullWidth />
+            <TextField name="email" label="E-mail" variant="standard" onChange={handleChange} fullWidth />
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose} disabled={!formData.username || !formData.password || !formData.email}>Create</Button>
+        </DialogActions>
+      </Dialog>
+      {/* <Grid container spacing={1} class="potato">
+          <Grid item sm={12}>
+            <TextField name="username" label="Username" variant="standard" onChange={handleChange} fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField name="password" label="Password" type="password" variant="standard" onChange={handleChange} fullWidth />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField name="email" label="E-mail" variant="standard" onChange={handleChange} fullWidth />
           </Grid>
           <Grid item sm={12}>
           </Grid>
@@ -35,8 +69,8 @@ function SignUpForm() {
             <Button variant="" fullWidth
               disabled={!formData.username || !formData.password || !formData.email}>Sign In</Button>
           </Grid>
-        </Grid>
-      </div>
+        </Grid> */}
+    </div>
   );
 }
 
